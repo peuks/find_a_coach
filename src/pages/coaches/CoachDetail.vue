6a9137bb6e18fc3bc2f447fca2b50a7ce9179750
Vanmak David <59261819+peuks@users.noreply.github.com>
2021-09-11 19:31:44 +0200

    ADD :  Wiring Up Pages (#4)

---
+++ src/pages/coaches/CoachDetail.vue
@@ -0,0 +1,7 @@
+<template>
+  DETAILS FOR COACH
+  <router-view />
+  <router-link to="/coaches/c1/contact">
+    Contact
+  </router-link>
+</template>
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -3,6 +3,14 @@
     FILTER
   </section>
   <section>
+    <div class="controls">
+      <button>
+        Refresh
+      </button>
+      <router-link to="/register">
+        Register as Coach
+      </router-link>
+    </div>
     LIST OF COACHES
   </section>
 </template>
