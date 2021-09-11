12485b16721a19344ea5c4202056f48833f1a560
Vanmak David <59261819+peuks@users.noreply.github.com>
2021-09-11 17:13:40 +0200

    ADD :   Adding Route Page Components (#2)

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export { default as CoachDetail } from "./coaches/CoachDetail.vue";
+export { default as CoachesList } from "./coaches/CoachesList.vue";
+export { default as CoachesRegistration } from "./coaches/CoachesRegistration.vue";
+
+export { default as ContactCoach } from "./requests/ContactCoach.vue";
+export { default as NotFound } from "./requests/NotFound.vue";
+export { default as RequestReceived } from "./requests/RequestReceived.vue";
