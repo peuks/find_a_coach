aba9cbfe2a75cc0407f715ec8c686cceb2029116
Vanmak David <59261819+peuks@users.noreply.github.com>
2021-09-12 14:31:16 +0200

    ADD : Add and set BaseComponents such as BaseBadge.vue BaseButton.vue and BaseCard.vue (#6)

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -3,27 +3,35 @@
     FILTER
   </section>
   <section>
-    <div class="controls">
-      <button>
-        Refresh
-      </button>
-      <router-link to="/register">
-        Register as Coach
-      </router-link>
-    </div>
-    <ul v-if="hasCoaches">
-      <coach-item
-        v-for="coach in filteredCoaches"
-        :key="coach.id"
-        :id="coach.id"
-        :first-name="coach.firstName"
-        :last-name="coach.lastName"
-        :rate="coach.hourlyRate"
-        :areas="coach.areas"
-      >
-      </coach-item>
-    </ul>
-    <h3 v-else>No coaches found.</h3>
+    <base-card>
+      <div class="controls">
+        <!-- <button>
+          Refresh
+        </button>
+        <router-link to="/register">
+          Register as Coach
+        </router-link> -->
+        <base-button mode="outline">
+          Refresh
+        </base-button>
+        <base-button to="/register">
+          Register as Coach
+        </base-button>
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :id="coach.id"
+          :first-name="coach.firstName"
+          :last-name="coach.lastName"
+          :rate="coach.hourlyRate"
+          :areas="coach.areas"
+        >
+        </coach-item>
+      </ul>
+      <h3 v-else>No coaches found.</h3>
+    </base-card>
     <!-- <h3 v-else>No coaches found.</h3> -->
   </section>
 </template>
@@ -31,7 +39,9 @@
 <script>
 import CoachItem from "../../components/coaches/CoachItem.vue";
 export default {
-  components: { CoachItem },
+  components: {
+    CoachItem,
+  },
   computed: {
     /**
        *We do have a getter to get coaches in a namespace !
